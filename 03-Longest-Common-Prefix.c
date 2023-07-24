a8732a6fc210e3e066caf6a44696e97f8bf052d5
Monles <iceueb@gmail.com>
2023-07-24 04:52:40 +0000

    update

--- 03-Longest-Common-Prefix.c
+++ 03-Longest-Common-Prefix.c
@@ -1,17 +1,35 @@
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& v) {
+char * longestCommonPrefix(char ** strs, int strsSize){
+    if (strsSize == 0) return "";
+    if (strsSize == 1) return strs[0];
 
-        string ans="";
-        sort(v.begin(), v.end());
-        int n = v.size();
-        string first = v[0], last=v[n-1];
-        for(int i=0; i < min(first.size(), last.size()); i++){
-          if(first[i] != last[i]){
-            return ans;
+    int len=0, min = strlen(strs[0]);
+    for(int i=1; i<strsSize; i++){
+      min = strlen(strs[i]) < min ? strlen(strs[i]) : min; 
+    }
+    
+    
+    for(int i =0;i < min; i++){
+      for(int j = 1; j < strsSize - 1; j++ ){
+          if(strs[i][j] == strs[i+1][j]){
+            if(i == strsSize - 2){
+              ++len;
+            }
+          }else{
+            goto br;
           }
-          ans += first[i];
-        }
-return ans;
-        }
-};
+
+
+      }
+    }
+
+    br:
+
+    if(len > 0){
+      char* ret = malloc(len * sizeof(int));
+      strs[0][len] = '\0';
+      strcpy(ret, strs[0]);
+      return ret;
+    }
+
+  return "";
+}
